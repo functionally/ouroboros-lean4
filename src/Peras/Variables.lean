538a3f5181db45847f50bd9e4459b02adf606ae3
Brian W Bush <code@functionally.io>
2024-09-21 14:11:17 -0600

    Specified blocks and chains.

--- src/Peras/Variables.lean
+++ src/Peras/Variables.lean
@@ -6,117 +6,73 @@ namespace Peras.Variables
 
 
 def Slot : Type := Nat
-deriving Repr, DecidableEq, BEq, Ord, Add, Sub, Mul, Div
+deriving Repr, DecidableEq, BEq, LT, Add, Sub, Mul, Div
 
 
 def Round : Type := Nat
-deriving Repr, DecidableEq, BEq, Ord, Add, Sub, Mul, Div
+deriving Repr, DecidableEq, BEq, LT, Add, Sub, Mul, Div
 
 
-class IsBlock (bl : Type) where
-  genesis : bl
-  make : bl
-  eq : DecidableEq bl
-
-instance [IsBlock bl] : Inhabited bl where
-  default := IsBlock.genesis
+variable {Party : Type}
+[DecidableEq Party]
 
 
-class IsChain (ch : Type) where
-  genesis : ch
---extend [IsBlock bl] : bl → ch → ch
---eq : DecidableEq ch
+variable {BlockHash : Type}
+[instInhabitedBlockHash : Inhabited BlockHash]
+[DecidableEq BlockHash]
 
-instance [IsChain ch] : Inhabited ch where
-  default := IsChain.genesis
-
-
-class IsVote (vo : Type) where
-  make : vo
-  eq : DecidableEq vo
+def genesisBlockHash : BlockHash :=
+  Inhabited.default
 
 
-class IsCert (ce : Type) where
-  genesis : ce
-  make : ce
-  eq : DecidableEq ce
+class IsBlock (Block : Type) where
+  create : Slot → Party → BlockHash → Block
+  slot : Block → Slot
+  creator : Block → Party
+  parent : Block → BlockHash
+  hash : Block → BlockHash
+  create_creator : ∀ sl pa bh, creator (create sl pa bh) = pa
+  create_slot : ∀ sl pa bh, slot (create sl pa bh) = sl
+  create_parent : ∀ sl pa bh, parent (create sl pa bh) = bh
+  not_genesis_hash : ¬ hash block = genesisBlockHash
 
-instance [IsCert ce] : Inhabited ce where
-  default := IsCert.genesis
+variable {Block : Type}
+[instBlock : @IsBlock Party BlockHash instInhabitedBlockHash Block]
 
 
-structure State where
-  Block : Type
-  Chain : Type
-  Vote : Type
-  Cert : Type
-  instChain : IsChain Chain
+class IsChain (Chain : Type) where
+  genesis : Chain
+  tipSlot : Chain → Slot
+  tipHash : Chain → BlockHash
+  extend (bl : Block) (ch : Chain) : tipSlot ch < instBlock.slot bl ∧ tipHash ch = instBlock.parent bl → Chain
+  expand (ch : Chain) : ¬ ch = genesis → Block × Chain
+  eq : DecidableEq Chain
+  extend_not_genesis : ∀ bl ch h, ¬ extend bl ch h = genesis
+  extend_expand : ∀ bl ch h, expand (extend bl ch h) (by apply extend_not_genesis) = ⟨ bl , ch ⟩
+
+variable {Chain : Type}
+[instChain : @IsChain Party BlockHash instInhabitedBlockHash Block instBlock Chain]
+
+
+class IsVote (Vote : Type) where
+  make : Vote
+  eq : DecidableEq Vote
+
+variable {Vote : Type}
+
+
+class IsCert (Cert : Type) where
+  genesis : Cert
+  make : Cert
+  eq : DecidableEq Cert
+
+
+class IsState (State : Type) where
   chainPref : Chain
   chains : Set Chain
   votes : Set Vote
   certPrime : Cert
   certStar : Cert
-
-def initial (st : State) : Prop :=
-  st.chainPref = @IsChain.genesis st.Chain st.instChain
-
-
-class IsState (Ch St : Type) where
-  instChain : St → IsChain Ch
-  chainPref : St → Ch
-
-def is_initial [IsState Ch St] (st : St) : Prop :=
-  IsState.chainPref st = @IsChain.genesis Ch (IsState.instChain st)
-
-
-class IsState' (Ch St : Type) where
-  instChain : St → IsChain Ch
-  chainPref : St → Ch
-
-def is_initial' [IsState' Ch St] [IsChain Ch] (st : St) : Prop :=
-  IsState'.chainPref st = (IsChain.genesis : Ch)
-
-
-class IsState'' (Ch St : Type) [IsChain Ch] where
-  chainPref : St → Ch
-
-def is_initial'' [IsChain Ch] [IsState'' Ch St] (st : St) : Prop :=
-  IsState''.chainPref st = (IsChain.genesis : Ch)
-
-
-variable {Ch : Type}
-[IsChain Ch]
-
-class IsState''' (St : Type) where
-  chainPref : St → Ch
-
-#check @IsState'''
-
-def is_initial''' [@IsState''' Ch St] (st : St) : Prop :=
-  IsState'''.chainPref st = (IsChain.genesis : Ch)
-
-#check is_initial'''
-
-
-inductive Chain where
-| gen : Chain
-| mk : Nat → Chain
-| oth : Chain
-
-instance iChain : IsChain Chain where
-  genesis := Chain.gen
-
-structure State' where
-  only : Chain
-
-instance iState : @IsState''' Chain State' where
-  chainPref st := st.only
-
-example : @is_initial''' Chain iChain State' iState ⟨ Chain.gen ⟩ := by
-  rfl
-
-example : @is_initial''' Chain iChain State' iState ⟨ Chain.oth ⟩ := by
-  rfl
 
 
 end Peras.Variables
