db7442c9561e3727d680ab1de858958d547d3eb8
Brian W Bush <consult@brianwbush.info>
2024-10-05 18:35:33 -0600

    Decidable Hoare triple.

--- praos/src/Praos/Spec.lean
+++ praos/src/Praos/Spec.lean
@@ -1,8 +1,6 @@
 
-import Hoare
 import Std.Data.HashMap
 
-open Hoare (HoareM)
 open Std
 
 
@@ -130,16 +128,12 @@ namespace BlockTree
   def insert (bt : BlockTree) (bl : Block) : BlockTree :=
     if bt.blocks.contains bl.hash
       then bt
-      else match bl with
-           | Block.genesis => Inhabited.default
-           | bl'@h₀:(Block.extend _ _ _ _ _) =>
-                    have h : bl' ≠ Block.genesis := by
-                      rw [h₀]
-                      apply Block.noConfusion
-                    let ph := bl'.parent h
-                    let pw := match bt.blocks.find? ph with
-                              | none => 0
-                              | some ⟨_, w⟩ => w
+      else if h : bl = Block.genesis
+             then Inhabited.default
+             else let ph := bl.parent h
+                  let pw := match bt.blocks.find? ph with
+                            | none => 0
+                            | some ⟨_, w⟩ => w
            {
              bt with
                blocks := bt.blocks.insert bl.hash ⟨bl, pw + 1⟩
@@ -212,35 +206,68 @@ deriving Repr, DecidableEq, BEq
 abbrev State (Node : Type) := Context × Node
 
 
-def Ticked (Node : Type) [IsNode Node] : Hoare.Post (State Node) (Option Block)
-| _, _, ⟨cx, no⟩ =>
-    let ob := IsNode.observe no
-    let noClock := ob.clock
-    let cxClock := cx.clock
-    cxClock = noClock
+structure HoareDec (σ α : Type) where
+  pre : σ → Bool
+  run : σ → α × σ
+  post : σ → α → σ → Bool
 
-def ticked (Node : Type) [IsNode Node] : HoareM HoareM.Always (Option Block) (Ticked Node) :=
+namespace HoareDec
+
+  def valid {σ α : Type} (h : HoareDec σ α) (s : σ) : Bool :=
+    let ⟨x, s'⟩ := h.run s
+    h.pre s → h.post s x s'
+
+  structure Result (σ α : Type) where
+    output : Option α
+    state : σ
+    validity : Bool
+
+  def step {σ α : Type} (h : HoareDec σ α) (s : σ) : Result σ α :=
+    let ⟨x, s'⟩ := h.run s
+    if h.pre s
+      then ⟨some x, s', h.post s x s'⟩
+      else ⟨none, s, true⟩
+
+end HoareDec
+
+
+def ticked (Node : Type) [IsNode Node] : HoareDec (State Node) (Option Block) :=
   {
-    run := fun ⟨cx, no⟩ ↦
+    pre := fun _ ↦ true
+  , run := fun ⟨cx, no⟩ ↦
       let cx' := cx.tick
       let ⟨out, no'⟩ := IsNode.tick no
       ⟨out, ⟨Option.elim out cx' cx'.receive, no'⟩⟩
-  , valid := sorry
+  , post := fun _ _ ⟨cx', no'⟩ ↦
+      let ob := IsNode.observe no'
+      let noClock := ob.clock
+      let cxClock := cx'.clock
+      cxClock = noClock
   }
 
-#check ticked
 
-instance : Decidable (HoareM.Always ()) where
+structure NodeEx where
+  clock : Slot
+deriving Repr
 
-#check HoareM.Always ()
+instance : IsNode NodeEx where
+  init := ⟨0⟩
+  tick no := ⟨none, ⟨no.clock + 1⟩⟩
+  receive _ := id
+  submit _ := id
+  observe no := ⟨ no.clock , genesisBlockHash ⟩
 
-def z : Hoare.Pre Unit := HoareM.Always
-#check z
-#eval decide z
 
-structure HoareM' {σ : Type} (p : Hoare.Pre σ) (α : Type) (q : Hoare.Post σ α) where
-  run : σ → α × σ
-  valid : ∀ s : σ, match run s with | ⟨x, s'⟩ => decide (p s) → decide (q s x s')
+def st0 : State NodeEx := ⟨Inhabited.default, IsNode.init⟩
+#eval st0.snd
+
+def ticked' := ticked NodeEx
+
+def step1 := ticked'.step st0
+def st1 := step1.state
+#eval step1.output
+#eval st1.snd
+#eval step1.validity
 
 
 end Praos.Spec
